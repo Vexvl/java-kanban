22f979715372df56907194e03946d8c0bcb19c0e
Vlad <vladucha09@inbox.ru>
2023-03-07 05:39:03 +0300

    sprint7v4

--- tests/service/FileBackedTasksManagerTest.java
+++ tests/service/FileBackedTasksManagerTest.java
@@ -26,14 +26,10 @@ class FileBackedTasksManagerTest extends TaskManagerTest<FileBackedTasksManager>
         this.taskManager = new FileBackedTasksManager(new File(PATH));
     }
 
-    @Test
-    public void createTask() throws ManagerSaveException, IOException, ManagerReadException {
-        List<Task> tasks = new ArrayList<>();
-        taskManager.createTask("Задача№1", "ОписаниеЗадача№1", 30, "2021-12-21T21:21:21");
-        tasks.add(taskManager.getTask(1));
-        historyManager.add(taskManager.getTask(1));
+    private void saveFile(List<Task> tasks) throws IOException {
         File dir = new File(BASE_DIRECTORY);
         dir.mkdir();
+
         File historyFile = new File(dir, FILE_NAME);
 
         FileWriter fileWriter = new FileWriter(historyFile);
@@ -43,13 +39,23 @@ class FileBackedTasksManagerTest extends TaskManagerTest<FileBackedTasksManager>
         if (br.readLine() == null) {
             fileWriter.write("id,type,name,status,description,epic,startTime,minutesToDo,endTime" + "\n");
         }
+
         for (Task task : tasks) {
             fileWriter.write(task.toString() + "\n");
         }
+
         fileWriter.write("\n");
         fileWriter.write(historyToString((historyManager)));
         fileWriter.close();
+    }
 
+    @Test
+    public void createTask() throws ManagerSaveException, IOException, ManagerReadException {
+        List<Task> tasks = new ArrayList<>();
+        taskManager.createTask("Задача№1", "ОписаниеЗадача№1", 30, "2021-12-21T21:21:21");
+        tasks.add(taskManager.getTask(1));
+        historyManager.add(taskManager.getTask(1));
+        saveFile(tasks);
         TaskManager fileBackedTasksManager = loadFromFile(new File(PATH));
         assertEquals(fileBackedTasksManager.getTaskById(1), taskManager.getTask(1));
     }
@@ -60,24 +66,7 @@ class FileBackedTasksManagerTest extends TaskManagerTest<FileBackedTasksManager>
         taskManager.createEpic("Эпик1", "ОписаниеЭпик№1");
         tasks.add(taskManager.getEpic(1));
         historyManager.add(taskManager.getEpic(1));
-        File dir = new File(BASE_DIRECTORY);
-        dir.mkdir();
-        File historyFile = new File(dir, FILE_NAME);
-
-        FileWriter fileWriter = new FileWriter(historyFile);
-        FileReader fileReader = new FileReader(historyFile);
-        BufferedReader br = new BufferedReader(fileReader);
-
-        if (br.readLine() == null) {
-            fileWriter.write("id,type,name,status,description,epic,startTime,minutesToDo,endTime" + "\n");
-        }
-        for (Task task : tasks) {
-            fileWriter.write(task.toString() + "\n");
-        }
-        fileWriter.write("\n");
-        fileWriter.write(historyToString((historyManager)));
-        fileWriter.close();
-
+        saveFile(tasks);
         TaskManager fileBackedTasksManager = loadFromFile(new File(PATH));
         assertEquals(taskManager.getEpic(1), fileBackedTasksManager.getEpic(1));
     }
@@ -89,24 +78,7 @@ class FileBackedTasksManagerTest extends TaskManagerTest<FileBackedTasksManager>
         taskManager.createSubTask("Подзадача1", "ОписаниеПодзадача1", 1, 3, "2056-12-21T21:21:21");
         tasks.add(taskManager.getEpic(1));
         historyManager.add(taskManager.getEpic(1));
-        File dir = new File(BASE_DIRECTORY);
-        dir.mkdir();
-        File historyFile = new File(dir, FILE_NAME);
-
-        FileWriter fileWriter = new FileWriter(historyFile);
-        FileReader fileReader = new FileReader(historyFile);
-        BufferedReader br = new BufferedReader(fileReader);
-
-        if (br.readLine() == null) {
-            fileWriter.write("id,type,name,status,description,epic,startTime,minutesToDo,endTime" + "\n");
-        }
-        for (Task task : tasks) {
-            fileWriter.write(task.toString() + "\n");
-        }
-        fileWriter.write("\n");
-        fileWriter.write(historyToString((historyManager)));
-        fileWriter.close();
-
+        saveFile(tasks);
         TaskManager fileBackedTasksManager = loadFromFile(new File(PATH));
         assertEquals(taskManager.getSubTaskById(1), fileBackedTasksManager.getSubtask(1));
     }
@@ -114,42 +86,20 @@ class FileBackedTasksManagerTest extends TaskManagerTest<FileBackedTasksManager>
 
     @Test
     public void save() throws IOException, ManagerReadException, ManagerSaveException {
-
         List<Task> tasks = new ArrayList<>();
-
         taskManager.createTask("Задача№1", "ОписаниеЗадача№1", 1, "2029-12-21T21:21:21");
         taskManager.createTask("Задача№2", "ОписаниеЗадача№1", 2, "2029-12-21T21:21:21");
         taskManager.createTask("Задача№3", "ОписаниеЗадача№1", 3, "2029-12-21T21:21:21");
         taskManager.createTask("Задача№4", "ОписаниеЗадача№1", 3, "2029-12-21T21:21:21");
-
         tasks.add(taskManager.getTask(1));
         tasks.add(taskManager.getTask(2));
         tasks.add(taskManager.getTask(3));
         tasks.add(taskManager.getTask(4));
-
         historyManager.add(taskManager.getTask(1));
         historyManager.add(taskManager.getTask(2));
         historyManager.add(taskManager.getTask(3));
         historyManager.add(taskManager.getTask(4));
-
-        File dir = new File(BASE_DIRECTORY);
-        dir.mkdir();
-        File historyFile = new File(dir, FILE_NAME);
-
-        FileWriter fileWriter = new FileWriter(historyFile);
-        FileReader fileReader = new FileReader(historyFile);
-        BufferedReader br = new BufferedReader(fileReader);
-
-        if (br.readLine() == null) {
-            fileWriter.write("id,type,name,status,description,epic,startTime,minutesToDo,endTime" + "\n");
-        }
-        for (Task task : tasks) {
-            fileWriter.write(task.toString() + "\n");
-        }
-        fileWriter.write("\n");
-        fileWriter.write(historyToString((historyManager)));
-        fileWriter.close();
-
+        saveFile(tasks);
         FileBackedTasksManager fileBackedTasksManager = loadFromFile(new File(PATH));
         assertEquals(4, fileBackedTasksManager.getHistory().size());
     }
@@ -160,24 +110,7 @@ class FileBackedTasksManagerTest extends TaskManagerTest<FileBackedTasksManager>
         taskManager.createTask("Задача№1", "ОписаниеЗадача№1", 30, "2021-12-21T21:21:21");
         tasks.add(taskManager.getTask(1));
         historyManager.add(taskManager.getTask(1));
-        File dir = new File(BASE_DIRECTORY);
-        dir.mkdir();
-        File historyFile = new File(dir, FILE_NAME);
-
-        FileWriter fileWriter = new FileWriter(historyFile);
-        FileReader fileReader = new FileReader(historyFile);
-        BufferedReader br = new BufferedReader(fileReader);
-
-        if (br.readLine() == null) {
-            fileWriter.write("id,type,name,status,description,epic,startTime,minutesToDo,endTime" + "\n");
-        }
-        for (Task task : tasks) {
-            fileWriter.write(task.toString() + "\n");
-        }
-        fileWriter.write("\n");
-        fileWriter.write(historyToString((historyManager)));
-        fileWriter.close();
-
+        saveFile(tasks);
         TaskManager fileBackedTasksManager = loadFromFile(new File(PATH));
         fileBackedTasksManager.deleteAll();
         assertEquals(0, fileBackedTasksManager.getAllTasks().size());
@@ -189,24 +122,7 @@ class FileBackedTasksManagerTest extends TaskManagerTest<FileBackedTasksManager>
         taskManager.createEpic("Эпик1", "ОписаниеЭпик№1");
         tasks.add(taskManager.getEpic(1));
         historyManager.add(taskManager.getEpic(1));
-        File dir = new File(BASE_DIRECTORY);
-        dir.mkdir();
-        File historyFile = new File(dir, FILE_NAME);
-
-        FileWriter fileWriter = new FileWriter(historyFile);
-        FileReader fileReader = new FileReader(historyFile);
-        BufferedReader br = new BufferedReader(fileReader);
-
-        if (br.readLine() == null) {
-            fileWriter.write("id,type,name,status,description,epic,startTime,minutesToDo,endTime" + "\n");
-        }
-        for (Task task : tasks) {
-            fileWriter.write(task.toString() + "\n");
-        }
-        fileWriter.write("\n");
-        fileWriter.write(historyToString((historyManager)));
-        fileWriter.close();
-
+        saveFile(tasks);
         TaskManager fileBackedTasksManager = loadFromFile(new File(PATH));
         taskManager.getEpic(1).getSubtasks().clear();
         assertEquals(0, fileBackedTasksManager.getEpic(1).getSubtasks().size());
@@ -218,24 +134,7 @@ class FileBackedTasksManagerTest extends TaskManagerTest<FileBackedTasksManager>
         taskManager.createEpic("Эпик1", "ОписаниеЭпик№1");
         tasks.add(taskManager.getEpic(1));
         historyManager.add(taskManager.getEpic(1));
-        File dir = new File(BASE_DIRECTORY);
-        dir.mkdir();
-        File historyFile = new File(dir, FILE_NAME);
-
-        FileWriter fileWriter = new FileWriter(historyFile);
-        FileReader fileReader = new FileReader(historyFile);
-        BufferedReader br = new BufferedReader(fileReader);
-
-        if (br.readLine() == null) {
-            fileWriter.write("id,type,name,status,description,epic,startTime,minutesToDo,endTime" + "\n");
-        }
-        for (Task task : tasks) {
-            fileWriter.write(task.toString() + "\n");
-        }
-        fileWriter.write("\n");
-        fileWriter.write(historyToString((historyManager)));
-        fileWriter.close();
-
+        saveFile(tasks);
         FileBackedTasksManager fileBackedTasksManager = loadFromFile(new File(PATH));
         taskManager.getEpic(1).getSubtasks().clear();
         fileBackedTasksManager.getHistory().clear();
